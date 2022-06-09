946c733573bd3058c5a0a764cc33af23c0eaf59d
Aliyo254 <alinurali254@gmail.com>
2022-06-09 13:14:47 +0300

    image and comment model

--- InstaClone/urls.py
+++ InstaClone/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, re_path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('instagram.urls')),
+    re_path(r'^accounts/', include('registration.backends.simple.urls')),
 ]
