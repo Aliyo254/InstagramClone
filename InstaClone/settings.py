24e09dafe9a8cdec490c67b7252ab4f1df25ba40
Aliyo254 <alinurali254@gmail.com>
2022-06-09 16:13:08 +0300

    Front-end complete

--- InstaClone/settings.py
+++ InstaClone/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'instagram',
     'bootstrap3',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'InstaClone.urls'
--- InstaClone/urls.py
+++ InstaClone/urls.py
@@ -18,8 +18,10 @@ from django.conf.urls import  url,include
 from django.contrib.auth import views
 
 urlpatterns = [
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
     url(r'^admin/', admin.site.urls),
     url(r'^',include('instagram.urls')),
+    url(r'^tinymce/', include('tinymce.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
